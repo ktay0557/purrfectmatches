f2707e4acaf4cd2cde5105b521d6ee3559c96171
Kayleigh Taylor <ktay0557@gmail.com>
2024-04-07 08:36:23 +0000

    Add image validation for Advert posting

--- adverts/serializers.py
+++ adverts/serializers.py
@@ -5,6 +5,21 @@ class AdvertSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
 
+    def validate_image(self, value):
+        if value.size > 1024 * 1024 * 2: # 2MB in bytes
+            raise serializers.ValidationError(
+                "Image size larger than 2MB!"
+            )
+        if value.image.width > 500:
+            raise serializers.ValidationError(
+                "Image width is larger than 500px"
+            )
+        if value.image.height > 600:
+            raise serializers.ValidationError(
+                "Image height is larger than 600px"
+            )
+        return value
+
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
