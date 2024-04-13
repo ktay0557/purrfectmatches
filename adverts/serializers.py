ea037390b21ab26e5fa5e2ca6c495c874d0023f1
Kayleigh Taylor <ktay0557@gmail.com>
2024-04-13 11:13:06 +0000

    Add extra fields for likes and adoption forms in advert views

--- adverts/serializers.py
+++ adverts/serializers.py
@@ -1,9 +1,13 @@
 from rest_framework import serializers
 from .models import Adverts
+from likes.models import Likes
+from adoptions.models import Adoption
 
 class AdvertSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    like_id = serializers.SerializerMethodField()
+    adoption_id = serializers.SerializerMethodField()
 
     def validate_image(self, value):
         if value.size > 1024 * 1024 * 2: # 2MB in bytes
@@ -24,6 +28,24 @@ class AdvertSerializer(serializers.ModelSerializer):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = Likes.objects.filter(
+                owner=user, advert=obj
+            ).first()
+            return like.id if like else None
+        return None
+
+    def get_adoption_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            adoption = Adoption.objects.filter(
+                owner=user, advert=obj
+            ).first()
+            return adoption.id if adoption else None
+        return None
+
     class Meta:
         model = Adverts
         fields = [
@@ -40,5 +62,7 @@ class AdvertSerializer(serializers.ModelSerializer):
             'other_animals',
             'content',
             'image',
-            'is_owner'
+            'is_owner',
+            'like_id',
+            'adoption_id',
         ]
