ce58203e9273e730f0051508192aff4f345c5c9f
Kayleigh Taylor <ktay0557@gmail.com>
2024-04-14 16:18:26 +0000

    Revert "Fix dj-rest-auth logout bug"
    This reverts commit 3650ee97652091a926fddc29b15d2d35481a9c06.

--- purrfectmatches/urls.py
+++ purrfectmatches/urls.py
@@ -21,7 +21,6 @@ urlpatterns = [
     path('', root_route),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('dj-rest-auth/logout/', logout_route),
     path('api-auth/', include('dj_rest_auth.urls')),
     path(
         'dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
