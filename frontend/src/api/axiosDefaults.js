6d529f4c45735b7124969887f306b9fed36a718e
Kayleigh Taylor <ktay0557@gmail.com>
2024-04-14 16:17:41 +0000

    Revert "Create User Sign Up form"
    This reverts commit 4e472cf7070923fc2f62791727bc28b877fca9d6.

--- frontend/src/api/axiosDefaults.js
+++ frontend/src/api/axiosDefaults.js
@@ -7,6 +7,5 @@
 
 import axios from "axios";
 
-axios.defaults.baseURL = "/api";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
--- frontend/src/pages/auth/SignUpForm.js
+++ frontend/src/pages/auth/SignUpForm.js
@@ -1,55 +1,20 @@
-import React, { useState } from "react";
-import { Link, useHistory } from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import Image from "react-bootstrap/Image";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
-import Container from "react-bootstrap/Container";
-import Alert from "react-bootstrap/Alert";
-import axios from "axios";
+import { Form, Button, Image, Col, Row, Container } from "react-bootstrap";
 
 const SignUpForm = () => {
-  const [signUpData, setSignUpData] = useState({
-    username: '',
-    password1: '',
-    password2: '',
-  });
-  const { username, password1, password2 } = signUpData;
-
-  const [errors, setErrors] = useState({});
-
-  const history = useHistory();
-
-  const handleChange = (event) => {
-    setSignUpData({
-      ...signUpData,
-      [event.target.name]: event.target.value,
-    });
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      await axios.post('/dj-rest-auth/registration/', signUpData);
-      history.push('/signin');
-    } catch (err) {
-      setErrors(err.response?.data);
-    }
-  };
-
   return (
     <Row className={styles.Row}>
       <Col className="my-auto py-2 p-md-2" md={6}>
         <Container className={`${appStyles.Content} p-4 `}>
           <h1 className={styles.Header}>sign up</h1>
 
-          <Form onSubmit={handleSubmit}>
+          <Form>
             <Form.Group controlId="username">
               <Form.Label className="d-none">Username</Form.Label>
               <Form.Control 
@@ -57,15 +22,8 @@ const SignUpForm = () => {
                 type="text" 
                 placeholder="Username" 
                 name="username"
-                value={username}
-                onChange={handleChange}
               />
             </Form.Group>
-            {errors.username?.map((message, idx) => (
-              <Alert variant="warning" key={idx}>
-                {message}
-              </Alert>
-            ))}
 
             <Form.Group controlId="password1">
               <Form.Label className="d-none">Password</Form.Label>
@@ -74,15 +32,8 @@ const SignUpForm = () => {
                 type="password" 
                 placeholder="Password" 
                 name="password1"
-                value={password1}
-                onChange={handleChange}
               />
             </Form.Group>
-            {errors.password1?.map((message, idx) => (
-              <Alert key={idx} variant="warning">
-                {message}
-              </Alert>
-            ))}
 
             <Form.Group controlId="password2">
               <Form.Label className="d-none">Confirm Password</Form.Label>
@@ -91,15 +42,8 @@ const SignUpForm = () => {
                 type="password" 
                 placeholder="Confirm Password" 
                 name="password2"
-                value={password2}
-                onChange={handleChange}
               />
             </Form.Group>
-            {errors.password2?.map((message, idx) => (
-              <Alert key={idx} variant="warning">
-                {message}
-              </Alert>
-            ))}
 
             <Button 
               className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
