bf742ba61ac90fb331ceb3c1567d4869254a8043
Kayleigh Taylor <ktay0557@gmail.com>
2024-04-14 16:17:45 +0000

    Revert "Create Sign Up form for users to register"
    This reverts commit 7858138e8be2608298a54ec0ea275bb8270fd0c9.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,6 @@ import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
-import "./api/axiosDefaults";
-import SignUpForm from "./pages/auth/SignUpForm";
 
 function App() {
   return (
@@ -13,7 +11,7 @@ function App() {
         <Switch>
           <Route exact path="/" render={() => <h1>Home Page</h1>} />
           <Route exact path="/signin" render={() => <h1>Sign In</h1>} />
-          <Route exact path="/signup" render={() => <SignUpForm />} />
+          <Route exact path="/signup" render={() => <h1>Sign Up</h1>} />
           <Route render={() => <p>Page Not Found!</p>} />
         </Switch>
       </Container>
