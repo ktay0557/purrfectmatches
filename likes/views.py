30abe01d3da7f7406c0b0f63525f2d9fdd1b439f
Kayleigh Taylor <ktay0557@gmail.com>
2024-04-17 15:48:30 +0000

    Update filters for likes app

--- likes/views.py
+++ likes/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, permissions
+from django_filters.rest_framework import DjangoFilterBackend
 from purrfectmatches.permissions import IsOwnerOrReadOnly
 from .models import Likes
 from .serializers import LikeSerializer
@@ -8,6 +9,10 @@ class LikeList(generics.ListCreateAPIView):
     serializer_class = LikeSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Likes.objects.all()
+    filter_backends = [
+        DjangoFilterBackend,
+    ]
+    filterset_fields = ['advert']
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
