6d0370634afac4911840414b76b07b247a701626
Kayleigh Taylor <ktay0557@gmail.com>
2024-04-17 08:39:04 +0000

    Remove image validation from advert serializer

--- adverts/serializers.py
+++ adverts/serializers.py
@@ -11,21 +11,6 @@ class AdvertSerializer(serializers.ModelSerializer):
     likes_count = serializers.ReadOnlyField()
     comments_count = serializers.ReadOnlyField()
 
-    def validate_image(self, value):
-        if value.size > 1024 * 1024 * 2: # 2MB in bytes
-            raise serializers.ValidationError(
-                "Image size larger than 2MB!"
-            )
-        if value.image.width > 500:
-            raise serializers.ValidationError(
-                "Image width is larger than 500px"
-            )
-        if value.image.height > 600:
-            raise serializers.ValidationError(
-                "Image height is larger than 600px"
-            )
-        return value
-
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
