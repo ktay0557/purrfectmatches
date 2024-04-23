ae2a8c1f54cde8c49dfce1ecfe9419f4973eeb3e
Kayleigh Taylor <ktay0557@gmail.com>
2024-04-23 17:47:43 +0000

    Update adoption serializer to allow options for related adverts

--- adoptions/serializers.py
+++ adoptions/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
 from .models import Adoption
+from adverts.models import Adverts
 
 class AdoptionSerializer(serializers.ModelSerializer):
+    advert_id = serializers.PrimaryKeyRelatedField(queryset=Adverts.objects.all(), write_only=True)
+
     def create(self, validated_data):
         validated_data['owner'] = self.context['request'].user
         return super().create(validated_data)
@@ -11,10 +14,10 @@ class AdoptionSerializer(serializers.ModelSerializer):
         fields = [
             'id',
             'owner',
-            'advert',
+            'advert_id',
             'created_at',
             'name',
             'email',
             'mobile',
-            'content'
+            'content',
         ]
